df5aad8b7825a7bd44dfa01c657674b3c3d28101
Sofía A <sofiacfgsdam@gmail.com>
2025-03-16 18:51:19 +0100

    Implementación de los casos de prueba

---
+++ CursoTest.java
@@ -0,0 +1,14 @@
+package instituto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ PersonaTest.java
@@ -0,0 +1,14 @@
+package instituto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
